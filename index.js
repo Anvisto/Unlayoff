a7713a673ae92a269c21e80145077ccaf74b7609
Anvisto <130226491+Anvisto@users.noreply.github.com>
2023-04-10 20:15:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-<html>
-<head>
-<h1> This is demo for jenkins </h1>
-</head>
-</html>
+var x;
+
+var name = prompt("Please enter your Name and Surname","Harry Potter");
+var gender;
+var male;
+var female;
+
+if (gender == male)
+{
+    alert("Welcome, MR. " + name);
+}
+else if (gender == female)
+{
+    alert("Welcome, MS. " + name);
+}
